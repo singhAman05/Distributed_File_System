39197e3b7cd778dc332eaf1827490f230e281458
singhAman05 <amanshankarsingh05@gmail.com>
2024-07-07 18:39:00 +0530

    adding frontend to repo

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "warm-orange": "#FFA500",
+        "deep-red": "#C0392B",
+        "dark-chocolate-brown": "#4E342E",
+        "harvest-gold": "#ECA400",
+        "ghost-white": "#F7F7FF",
+        "chilli-red": "#E3170A",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
